2893de125ef3c74ac7641fcfd56af4481e2a88d7
amanka <amankumar.a@vvdntech.in>
2022-07-04 22:22:41 +0530

    added routes

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/amangogit/book-store
 go 1.14
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
 )
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/amango/book-store/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBooksStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBookById).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBookById).Methods("DELETE")
+}
