78a56a49c692a22186558691461d10c99602f11c
amangogit <aman.devacc@gmail.com>
2022-07-07 00:03:00 +0530

    added comment

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// parse json data in request body using Unmarshal
 func ParseBody(req *http.Request, x interface{}) {
 	if body, err := ioutil.ReadAll(req.Body); err != nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
