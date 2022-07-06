2219dd37bcfc1dfa29fdd8979898c4ed856bb9a2
amanka <amankumar.a@vvdntech.in>
2022-07-06 23:35:20 +0530

    function to initialize db

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -5,3 +5,18 @@ import (
 	"github.com/amango/book-store/pkg/config"
 )
 
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Name string `gorm:json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
