ad61dea5202a9f739aac55cf32c57fa591ebcb5a
amangogit <aman.devacc@gmail.com>
2022-07-11 02:09:40 +0530

    upadted err handling in utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -8,7 +8,7 @@ import (
 
 // parse json data in request body using Unmarshal
 func ParseBody(req *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(req.Body); err != nil {
+	if body, err := ioutil.ReadAll(req.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
